11060d306c60ceb835b2fb344c8c3be9dc096a4d
thy3368 <thy_email@126.com>
2025-02-09 21:44:03 +0800

    刀耕火种

--- src/domain/Price.rs
+++ src/domain/Price.rs
@@ -1,5 +1,24 @@
 pub struct Price {
     pub order_no: String,
-
 }
 
+impl Price {
+    // 构造函数
+    pub fn new(name: String, age: u32) -> Self {
+        Self { name, age }
+    }
+    // getter
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+    pub fn age(&self) -> u32 {
+        self.age
+    }
+    // setter
+    pub fn set_name(&mut self, name: String) {
+        self.name = name;
+    }
+    pub fn set_age(&mut self, age: u32) {
+        self.age = age;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::fmt::{Display, Formatter};
+
+struct Price {
+    order_no: String
+}
+
+impl Display for Price {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+
+    let order = Price { order_no: String::from("123456") };
+    println!("{}", order);
+
 }
